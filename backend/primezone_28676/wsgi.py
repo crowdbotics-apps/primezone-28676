e3442541e4dd26a0eba5605531a3e198dae71059
Crowdbotics <team@crowdbotics.com>
2021-07-09 23:01:50 +0000

    Initial commit.

---
+++ backend/primezone_28676/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for primezone_28676 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'primezone_28676.settings')
+
+application = get_wsgi_application()
