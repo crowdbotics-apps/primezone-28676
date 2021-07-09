331462388cd182f4b603673535f9b344ebd5de34
Crowdbotics <team@crowdbotics.com>
2021-07-09 23:06:13 +0000

    Update models

--- backend/primezone_28676/wsgi.py
+++ backend/primezone_28676/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'primezone_28676.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "primezone_28676.settings")
 
 application = get_wsgi_application()
